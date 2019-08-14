e975d73dc85fc6881507a40f6e7bbe0a4130aa9c
Stephen Jeong <stephen.h.jeong@gmail.com>
2019-08-13 20:12:15 -0700

    Pass data from get request as props to components

--- client/src/Components/Summary.jsx
+++ client/src/Components/Summary.jsx
@@ -5,7 +5,7 @@ const Summary = (props) => {
   return (
     <tr>
       <td>
-        <p className="summary">Shabby chic raclette kombucha, man bun tacos cloud bread distillery chillwave lomo sustainable wayfarers glossier freegan. Dreamcatcher street art +1 retro small batch tacos craft beer four loko sriracha shabby chic cloud bread art party literally narwhal PBR&B. Distillery meditation squid ennui. Marfa gochujang roof party, blue bottle fanny pack brooklyn bitters. Fashion axe polaroid microdosing, af swag cold-pressed plaid marfa copper mug butcher blog quinoa slow-carb.</p>
+        <p className="summary">{props.description}</p>
       </td>
     </tr>
   );
