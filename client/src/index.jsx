ea1fb148da93b82130c9aed2990cfad6cab50c4c
Stephen Jeong <stephen.h.jeong@gmail.com>
2019-08-27 22:36:35 -0700

    Change div id used in app to 'overview' instead of 'app'

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('App'));
+ReactDOM.render(<App />, document.getElementById('overview'));
