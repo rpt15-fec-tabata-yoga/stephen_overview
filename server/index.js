b609828a57b7c3750021a2c791a077217c6cd21c
Stephen Jeong <stephen.h.jeong@gmail.com>
2019-08-27 19:25:36 -0700

    Add extra line to server to show default page when no gameId is given

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const db = require('../db/index.js');
 const app = express();
 
 app.use('/:gameId', express.static('public'));
+app.use('/', express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
