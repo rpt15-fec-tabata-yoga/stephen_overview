9c65cc4d279b170875f49bd25c9ffcf04319d61d
Stephen Jeong <stephen.h.jeong@gmail.com>
2019-09-14 15:24:58 -0700

    Remove get requests for other peoples database in server file

--- server/index.js
+++ server/index.js
@@ -24,35 +24,6 @@ app.get(`/api/overview/:gameId`, (req, res) => {
   });
 });
 
-// app.get('/api/image/:gameId', (req, res) => {
-//   // make request to Bryan's server
-//   axios.get(`http://ec2-13-57-33-155.us-west-1.compute.amazonaws.com:3002/api/image/${req.params.gameId}/stardew_valley`)
-//   .then((response) => {
-//     // console.log('response.data from get request to Bryan', response.data);
-//     res.send(response.data);
-//   })
-//   .catch((error) => {
-//     console.log('error in get request to Bryan database', error);
-//   });
-// });
-
-// app.get('/api/reviews/:gameId', (req, res) => {
-//   // make request to Therese's server
-//   console.log('got to reviews get request in server');
-//   axios.get(`http://ec2-54-67-60-167.us-west-1.compute.amazonaws.com/api/reviews/${req.params.gameId}`)
-//     .then((response) => {
-//       console.log('response.data from get request to Therese', response.data);
-//       // error handle if there's no data
-//       if (response.data.length > 0) {
-//         res.send(response.data);
-//       }
-//     })
-//     .catch((error) => {
-//       console.log('error in get request to Therese database', error);
-//     });
-// });
-
-
 app.listen(port, () => {
   console.log(`App listening on ${port}`);
 });
