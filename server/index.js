d2201b15973e53807dac75e5fa30e5f169e45ad6
Stephen Jeong <stephen.h.jeong@gmail.com>
2019-09-19 19:25:52 -0700

    Change order of app.get in server file

--- server/index.js
+++ server/index.js
@@ -7,18 +7,17 @@ const port = 3000;
 
 const app = express();
 
+app.get('*.js', (req, res, next) => {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
 app.use(cors());
 app.use('/', express.static('public'));
 app.use('/:gameId', express.static('public'));
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('*.js', (req, res, next) => {
-  req.url = req.url + '.gz';
-  res.set('Content-Encoding', 'gzip');
-  next();
-});
 
 app.get(`/api/overview/:gameId`, (req, res) => {
   console.log('got to overview get request in server');
